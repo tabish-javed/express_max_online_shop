eee6e0896ce8478a39dfd9608bb1555d36b23af2
TABISH <tabishjaved@hotmail.com>
2024-12-19 00:34:06 +0530

    models added

--- controller/products.js
+++ controller/products.js
@@ -1,15 +1,18 @@
-const products = []
+import Product from "../models/product.js"
 
 function getAddProduct (req, res, next) {
   res.status(200).render('add_product', { docTitle: "Add Product", path: "/admin/add-product" })
 }
 
 function postAddProduct (req, res, next) {
-  products.push({ title: req.body.title })
+  const product = new Product.Product(req.body.title)
+  product.save()
   res.status(302).redirect("/")
 }
 
 function getProducts (req, res, next) {
+  const products = Product.Product.fetchAll()
+  console.log(products)
   res.status(200).render('shop', { products, docTitle: "Shop", path: "/" })
 }
 
@@ -18,5 +21,4 @@ export default {
   getAddProduct,
   postAddProduct,
   getProducts,
-  products
 }
